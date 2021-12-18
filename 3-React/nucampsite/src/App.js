4a819dd7d59bbb0b2812a29a03d749515ee98e62
Drake Collins <drakecollins95@outlook.com>
2021-12-18 06:44:04 -0700

    Components Part 2

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,9 +1,17 @@
+
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          campsites: CAMPSITES
+      };
+    }
     render() {
         return (
             <div className="App">
@@ -12,35 +20,11 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
 }
 
 export default App;
-/*import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
