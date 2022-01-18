51fc344db8e5365ffbb95061459359b68119a640
Drake Collins <drakecollins95@outlook.com>
2022-01-17 17:16:38 -0700

    Fetch Post Comment

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -30,7 +30,7 @@ function RenderCampsite({campsite}) {
         );
     }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -51,7 +51,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                                 </div>
                             );
                         })}
-                        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                        <CommentForm campsiteId={campsiteId} addComment={postComment} />
                 </div>
             );
         } 
@@ -98,7 +98,7 @@ function CampsiteInfoComponent(props) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                         />
                     </div>
@@ -137,7 +137,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
